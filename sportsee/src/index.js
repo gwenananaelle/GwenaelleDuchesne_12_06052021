f31e9f85ac8ccd2f18b74618bbd73c618a69f983
gwenaelle <gwenaelle.duchesne@gmail.com>
2022-03-21 18:28:30 +0100

    normalize and font in links rather than @import

--- sportsee/src/index.js
+++ sportsee/src/index.js
@@ -7,8 +7,6 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
     * {
-        @import-normalize;
-        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap');
         font-family: 'Roboto', sans-serif;
     }
 `
